71a3c7e103ddeac7fa847680745cadd37fef839f
Anisa <aagermanova@gmail.com>
2020-02-10 23:04:41 -0800

    Добавляет стилизацию блоков

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,34 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+/* eslint-disable one-var */
+/* eslint-disable no-undef */
+/* eslint-disable strict */
+$(function () {
+  var Accordion = function (el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
 
-pageHeader.classList.remove('page-header--nojs');
+    var dropdownlink = this.el.find('.accordion-menu__item-header');
+    dropdownlink.on('click',
+        {el: this.el, multiple: this.multiple},
+        this.dropdown);
+  };
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  Accordion.prototype.dropdown = function (e) {
+    var $el = e.data.el,
+      $this = $(this),
+      // this is the ul.submenuItems
+      $next = $this.next();
+
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+
+    if (!e.data.multiple) {
+      // show only one menu at the same time
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  var accordion = new Accordion($('.accordion-menu'), false);
+// eslint-disable-next-line semi
+})
